521a66fa661434109235807b51f06283c6d98af3
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-02-07 00:55:04 +0000

    primeros cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todolist, setTodolist] = useState([]);
+	const [newlist, setNewlist] = useState("");
+
+	useEffect(() => {
+		const getTodo = () => {
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/joaquinA", {
+				method: "GET",
+				body: JSON.stringify(getTodo),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				// las promesas
+				.then((response) => response.json())
+				.then((data) => setNewlist(data));
+		};
+		getTodo();
+	}, []);
+
+	useEffect(() => {
+		const getTodo = () => {
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/joaquinA", {
+				method: "PUT",
+				body: JSON.stringify(getTodo),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				// las promesas
+				.then((response) => response.json())
+				.then((data) => setNewlist(data));
+		};
+		getTodo();
+	}, []);
+
+	const updatelist = ({ target }) => {
+		// Update query onKeyPress of input box
+		setNewlist(target.value);
+	};
+
+	const Search = ({ newlist }) => <li>{newlist}</li>;
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<ul className="list-group">
+				<input onChange={updatelist}></input>
+				{todolist.map((newlist, i) => (
+					<Search
+						newlist={newlist}
+						// Prevent duplicate keys by appending index:
+						key={newlist + i}
+					/>
+				))}
+			</ul>
+			<button onClick={() => setTodolist([...todolist, newlist])}>
+				Add Item
+			</button>
 		</div>
 	);
 };
